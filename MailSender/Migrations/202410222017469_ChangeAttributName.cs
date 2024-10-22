68cd3650320210509989651f620f7d27abde4e29
mmakselon <magdalena.pachacz@gmail.com>
2024-10-22 22:40:43 +0200

    fix model-change attribut name

---
+++ MailSender/Migrations/202410222017469_ChangeAttributName.cs
@@ -0,0 +1,20 @@
+﻿namespace MailSender.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeAttributName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.EmailMessages", "SendDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.EmailMessages", "SentDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.EmailMessages", "SentDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.EmailMessages", "SendDate");
+        }
+    }
+}
