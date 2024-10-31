e6861b41d07c9af0498e109a0f998f7c8b16ca03
mmakselon <magdalena.pachacz@gmail.com>
2024-10-31 22:55:20 +0100

    changing model

---
+++ MailSender/Migrations/202410302133587_ChangingEmailMessages.cs
@@ -0,0 +1,20 @@
+﻿namespace MailSender.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangingEmailMessages : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.EmailAccountParams", "UserId", "dbo.AspNetUsers");
+            AddForeignKey("dbo.EmailAccountParams", "UserId", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.EmailAccountParams", "UserId", "dbo.AspNetUsers");
+            AddForeignKey("dbo.EmailAccountParams", "UserId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+        }
+    }
+}
